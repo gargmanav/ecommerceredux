b4cc317336aa46eebd64f16cc3d7a9e692936861
gargmanav <manavgarg1998@gmail.com>
2023-04-15 01:26:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import Home from './Components/Home';
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route, Navigate} from 'react-router-dom'
 import Products from './Components/Products';
 import Add_To_Cart from './Components/Add_to_cart';
 import { useState } from 'react';
+import Technologies from './Components/Technologies';
+import Html from './Components/Html';
+import Css from './Components/Css';
+import Javas from './Components/Javas';
+import Reactss from './Components/Reactss';
+import Login from './Components/Login';
 
 function App() {
   const [carddata, setcarddata] = useState([])
+  const [logdata,setlogdata] = useState(false)
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/products' element={<Products carddata={carddata} setcarddata={setcarddata}/>}/>
-        <Route path='/addtocart' element={<Add_To_Cart carddata={carddata} setcarddata={setcarddata}/>}/>
+        <Route path='/' element={logdata?<Navigate to="/home"/>:<Login setlogdata={setlogdata}/>}/>
+        <Route path='/home' element={logdata?<Home/>:<Navigate to="/"/>}/>
+        <Route path='/products' element={logdata?<Products carddata={carddata} setcarddata={setcarddata}/>:<Navigate to="/"/>}/>
+        <Route path='/addtocart' element={logdata?<Add_To_Cart carddata={carddata} setcarddata={setcarddata}/>:<Navigate to="/"/>}/>
+        <Route path='/technologies' element={logdata?<Technologies/>:<Navigate to="/"/>}>
+        <Route path='' element={<Navigate to="html"/>}/>
+        <Route path='html' element={<Html/>}/>
+        <Route path='css' element={<Css/>}/>
+        <Route path='js' element={<Javas/>}/>
+        <Route path='react' element={<Reactss/>}/>
+        </Route>
+       
       </Routes>
      
     </div>
@@ -20,3 +36,5 @@ function App() {
 }
 
 export default App;
+
+
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import "./navbar.css"
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setlogdata}) => {
   return (
     <>
     <div className='mainbox'>
         <div className='firstdiv'></div>
         <div className='seconddiv'>
             <ul>
-              <Link to="/"><li>Home</li></Link>
+              <Link to="/home"><li>Home</li></Link>
               <Link to="/products"><li>Products</li></Link>
               <Link to="/addtocart"><li>Add To Cart</li></Link>
+              <Link to="/technologies"><li>Technologies</li></Link>
+              <button onClick={setlogdata(false)}>Logout</button>
             </ul>
         </div>
     </div>
